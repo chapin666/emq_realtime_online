a6f5cb821c36d6573dc6af558ff91a69ad416830
ChengBin <997155658@qq.com>
2018-03-21 13:47:11 +0800

    update

--- src/emq_realtime_online.erl
+++ src/emq_realtime_online.erl
@@ -44,8 +44,8 @@ on_client_subscribe(ClientId, Username, TopicTable, _Env) ->
 on_client_unsubscribe(ClientId, Username, TopicTable, _Env) ->
     [H | _] = proplists:get_keys(TopicTable),
     case ets:lookup(?ONLINE_TAB, H) of
-        [{_, Val}] -> io:format("unsubscribe: Val=~p ~n", [Val]), ets:update_element(?ONLINE_TAB, H, {2, Val-1});
-        [{_, 0}] -> io:format("delete ~n"), ets:delete(?ONLINE_TAB, H)
+        [{_, 1}] -> io:format("delete ~n"), ets:delete(?ONLINE_TAB, H);
+        [{_, Val}] -> io:format("unsubscribe: Val=~p ~n", [Val]), ets:update_element(?ONLINE_TAB, H, {2, Val-1})
     end,
     {ok, TopicTable}.
 
