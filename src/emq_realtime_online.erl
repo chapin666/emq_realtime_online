c7c4a74a20d9e8cbff5be18d4d0ee4ed4e03e28d
ChengBin <997155658@qq.com>
2018-03-21 14:10:37 +0800

    update

--- src/emq_realtime_online.erl
+++ src/emq_realtime_online.erl
@@ -37,7 +37,7 @@ on_client_subscribe(ClientId, Username, TopicTable, _Env) ->
     [H | _] = proplists:get_keys(TopicTable),
     case ets:lookup(?ONLINE_TAB, H) of
         [{_, Val}] -> io:format("subscribe: Val=~p ~n", [Val]), ets:update_element(?ONLINE_TAB, H, {2, Val+1});
-        [] -> ets:insert(?ONLINE_TAB, {H, 1})
+        [] -> io:format("insert 1"), ets:insert(?ONLINE_TAB, {H, 1})
     end,
     {ok, TopicTable}.
     
